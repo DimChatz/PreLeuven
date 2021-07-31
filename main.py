753594c6befc2245368c389f57d54b09219159e4
DimChatz <37213442+DimChatz@users.noreply.github.com>
2021-07-31 18:35:22 +0200

    Add files via upload
    Final implementations

--- main.py
+++ main.py
@@ -11,18 +11,21 @@ from skimage.draw import disk
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
 from keras_unet_collection import models
+import os
+from tensorboard.plugins.hparams import api as hp
+
 
 #####################################################################
 ### Creating the dataloading function for the input and the masks ###
 #####################################################################
 
-def dataloader(filepath, subset):
+def dataloader_mias(filepath, subset):
 	# Initiliaze return arrays
 	global size
 	if subset=="train":
 		size = 96
 	elif subset=="val":
-		size = 18
+		size = 18 
 	elif subset=="test":
 		size = 18
 	elif subset=="all":
@@ -78,6 +81,19 @@ def dataloader(filepath, subset):
 	print(np.unique(output_data))
 	return input_data, output_data
 
+def dataloader_ddsm(filepath,size):
+	count = 0
+	#array for reading into
+	input_data=np.zeros((size,512,512,1),dtype=np.uint8)
+	# Read
+	for file in os.listdir(filepath):
+		# Find paths
+		test_path = os.path.join(filepath, file)
+		data=cv2.imread(test_path,0)
+		input_data[count,:,:,0] = data
+		count = count + 1
+	return input_data
+
 ###########################
 ### Definition of model ###
 ###########################
@@ -116,31 +132,31 @@ def decoder_block(input1, input2,filter_size, kernel_size):
 
 # Main model
 def unet_model(num_classes, optimizer, loss_metric, metrics, sample_width, sample_height, lr=1e-5):
-	inputs = Input((sample_width, sample_height, 1))
-	
-	# Downsampling
-	conv1, drop1 = encoder_block(inputs,32,3)
-	conv2, drop2 = encoder_block(drop1,64,3)
-	conv3, drop3 = encoder_block(drop2,128,3)
+    inputs = Input((sample_width, sample_height, 1))
+    
+    # Downsampling
+    conv1, drop1 = encoder_block(inputs,64,3)
+    conv2, drop2 = encoder_block(drop1,128,3)
+    conv3, drop3 = encoder_block(drop2,256,3)
+    conv4, drop4 = encoder_block(drop3,512,3)
+    
+    # Bottleneck
+    conv5 = bottleneck(drop4,1024,3)
 
-	
-	# Bottleneck
-	conv4 = bottleneck(drop3,256,3)
+    # Upsampling
+    conv6 = decoder_block(conv5,conv4,512,2)
+    conv7 = decoder_block(conv6,conv3,256,2)
+    conv8 = decoder_block(conv7,conv2,128,2)
+    conv9 = decoder_block(conv8,conv1,64,2)
 
-	# Upsampling
-	conv5 = decoder_block(conv4,conv3,128,2)
-	conv6 = decoder_block(conv5,conv2,64,2)
-	conv7 = decoder_block(conv6,conv1,32,2)
+    # Output
+    conv10 = Conv2D(num_classes, 1, padding='same', activation='softmax')(conv9)
 
+    model = Model(inputs=[inputs], outputs=[conv10])
 
-	# Output
-	conv7 = Conv2D(num_classes, 1, padding='same', activation='softmax')(conv7)
-	conv7 = Reshape((sample_width*sample_height,num_classes))(conv7)
+    model.compile(optimizer=optimizer(lr=lr), loss=loss_metric, metrics=metrics)
+    return model
 
-	model = Model(inputs=[inputs], outputs=[conv7])
-
-	model.compile(optimizer=optimizer(lr=lr), loss=loss_metric, metrics=metrics, sample_weight_mode="temporal")
-	return model
 
 ######################
 ### Util functions ###
@@ -188,7 +204,7 @@ def show_predictions(tensor_in,tensor_out, num=6, size=314):
 class DisplayCallback(tf.keras.callbacks.Callback):
 	def on_epoch_end(self, epoch, logs=None):
 		clear_output(wait=True)
-		show_predictions(train_input,train_output)
+		show_predictions(train_input,train_masks)
 
 
 def create_mask(pred_mask):
@@ -196,69 +212,70 @@ def create_mask(pred_mask):
 	return pred_mask
 
 def iou(y_true, y_pred):
-	smooth=0.001
-	intersection = K.sum(K.abs(y_true * y_pred), axis=[1,2])
-	union = K.sum(y_true,[1,2,3])+K.sum(y_pred,[1,2,3])-intersection
-	iou = K.mean((intersection + smooth) / (union + smooth), axis=0)
-	return iou
+	intersection = K.sum(y_true * y_pred)
+	union =  K.sum(y_true) +  K.sum(y_pred) - intersection
+	x = (intersection + 1e-15) / (union + 1e-15)
+	return x
 
-def iou_loss(y_true, y_preds):
+def iou_loss(y_true, y_pred):
 	return -iou(y_pred, y_true)
 
 #####################
 ### MAIN PROGRAM ####
 #####################
 
-
 # HYPERPARAMETERS
 # Filepath of datasets
-filepath = "/home/tzikos/Downloads/train/"
+filepath_train_img = "/home/bitu/Downloads/train/images"
+filepath_train_mask = "/home/bitu/Downloads/train/masks"
+filepath_test_img = "/home/bitu/Downloads/test/images"
+filepath_test_mask = "/home/bitu/Downloads/test/masks"
 
-# Variable to train model or load from wright
-train_model=True
+train_mode = False
 
-# Load datasets
-train_input, train_output = dataloader(filepath, "all")
-test_input, test_output = dataloader(filepath, "test")
-val_input, val_output = dataloader(filepath, "val")
-	
+
+# Get data
+train_input = dataloader_ddsm(filepath_train_img,1231)
+train_masks = dataloader_ddsm(filepath_train_mask,1231)
+test_input = dataloader_ddsm(filepath_test_img,361)
+test_masks = dataloader_ddsm(filepath_test_mask,361)
+
 # Normalize images
 train_input = normalize(train_input)
 test_input = normalize(test_input)
-val_input = normalize(val_input)
+test_masks = normalize(test_masks)
+train_masks = normalize(train_masks)
 
-	
-# Training option
-if train_model==True:
-	# Load model
-	unet = models.unet_2d((128, 128, 1), [32, 64, 128, 256], n_labels=2,
-                      stack_num_down=2, stack_num_up=2,
-                      activation='ReLU', output_activation='Softmax',
-                      batch_norm=True, pool=True, unpool=False, name='unet')
-	unet.compile(optimizer=Adam(lr=1e-3), loss='binary_crossentropy', metrics=['accuracy'])
-	unet.summary()
+if train_mode == True:
+    # Load model
+    unet = unet_model(num_classes = 2, optimizer = Adam, loss_metric = iou_loss, metrics = dice_coef, sample_width = test_input.shape[1], sample_height = test_input.shape[2], lr=1e-3)
+    unet.compile(optimizer=Adam(lr=1e-3), loss = iou_loss,  metrics=[dice_coef])
+    unet.summary()
 
-	# Define callbacks
-	callbacks = [
-	ModelCheckpoint(filepath="/home/tzikos/Thesis/unet.h5", monitor='val_loss', verbose=1, save_best_only=True, mode='min'),
-	ReduceLROnPlateau(monitor="val_loss", patience=2, factor=0.1, verbose=1, mode='min', min_lr=1e-8),
-	EarlyStopping(monitor="val_loss", patience=5, verbose=1),
-	DisplayCallback()
-	]
+    # Define callbacks
+    callbacks = [
+    ModelCheckpoint(filepath="C:/Users/xatzo/Downloads/unet_new.h5", monitor='val_loss', verbose=1, save_best_only=True, mode='min'),
+    ReduceLROnPlateau(monitor="val_loss", patience=2, factor=0.1, verbose=1, mode='min', min_lr=1e-8),
+    EarlyStopping(monitor="val_loss", patience=5, verbose=1),
+    DisplayCallback()
+    ]
 
-	show_predictions(train_input,train_output)
-	history = unet.fit(x=train_input, y=train_output, 
-		validation_data=(val_input,val_output), 
-	batch_size=4, epochs=50, callbacks=callbacks)
+    show_predictions(test_input,test_masks)
+    history = unet.fit(x=test_input, y=test_masks, 
+        validation_data=(test_input,test_masks), 
+    batch_size=1, epochs=50, callbacks=callbacks)
+    
 
-	# Test model
-	show_predictions(train_input, train_output, num=train_input.shape[0])
-	
-# Loading from weights option
+    # Test model
+    show_predictions(test_input, test_masks, num=test_input.shape[0])
+
 else:
-	# Initialize model from weights
-	model = unet_model(num_classes=1, optimizer=Adam, loss_metric=dice_coef_loss, metrics=[dice_coef], sample_width=train_input.shape[1], sample_height=train_input.shape[2],lr=1e-5)
-	model = load_model('/home/tzikos/Thesis/unet.h5', custom_objects = {"dice_coef": dice_coef,"dice_coef_loss": dice_coef_loss})
-
-	# Test model
-	show_predictions(test_input, test_output, num=test_input.shape[0])
+    # Initialize model from weights
+    unet = unet_model(num_classes=2, optimizer=Adam, loss_metric=iou_loss,
+                       metrics=[dice_coef], sample_width=train_input.shape[1], sample_height=train_input.shape[2],lr=1e-3)
+    unet = load_model('C:/Users/xatzo/Downloads/unet64_iou4_drop3.h5', custom_objects={"dice_coef": dice_coef, "dice_coef_loss": dice_coef_loss, "iou": iou, "iou_loss": iou_loss})
+    unet.summary()
+    
+    # Test model
+    show_predictions(train_input, train_masks, num=5)
+    show_predictions(test_input, test_masks, num=5)

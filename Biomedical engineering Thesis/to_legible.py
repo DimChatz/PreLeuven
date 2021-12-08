ea50b6da0605846566267906908a84f1742bfb43
DimChatz <37213442+DimChatz@users.noreply.github.com>
2021-12-08 20:04:33 +0100

    Update to_legible.py

--- Biomedical engineering Thesis/to_legible.py
+++ Biomedical engineering Thesis/to_legible.py
@@ -1,6 +1,11 @@
-###############################################################
-### Script for transforming the cbis-ddsm dataset into legible format ###
-###############################################################
+#########################################################################
+### Script for transforming the CBIS-DDSM dataset into legible format ###
+#########################################################################
+
+# The following analysis was done based on a 3 part preprocessing process I found at:
+# https://towardsdatascience.com/can-you-find-the-breast-tumours-part-1-of-3-1473ba685036
+# https://towardsdatascience.com/can-you-find-the-breast-tumours-part-2-of-3-1d43840707fc
+# https://towardsdatascience.com/can-you-find-the-breast-tumours-part-3-of-3-388324241035
 
 
 # Importing libraries
@@ -10,8 +15,8 @@ import os
 import pydicom as dicom
 import fnmatch
 
-
 # Croppig borders
+# l=left, r=right, u=upepr, d=down
 def cropBorders(img, l=0.02, r=0.02, u=0.04, d=0.04):
     nrows, ncols = img.shape
     # Get the start and end rows and columns
@@ -27,15 +32,23 @@ def minMaxNormalise(img):
     norm_img = (img - img.min()) / (img.max() - img.min())
     return norm_img
 
-# Binarise Image
+# Binarise Image to maxval
 def Binarise(img, thresh, maxval):
     binarised_img = np.zeros(img.shape, np.uint8)
     binarised_img[img >= thresh] = maxval
     return binarised_img
 
 # Expand to capture artefacts
+# ksize is the size of the window to be applied
+# operation can be "open" or "closed"
+# erosion and then dilation is performed on the image in the case of opening,
+# and the reverse in the case of closing. 
+# When erosion is employed, the kernel is used on each pixel in order to set it to the minimum of its kernel pixel neighbors 
+# while dilation sets it to the maximum.
+# In the case of the opening small bright spots are removed, while in the case of closing, dark pixels become bright
 def editMask(mask, ksize=(23, 23), operation="open"):
     kernel = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=ksize)
+    # Choose operation
     if operation == "open":
         edited_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     elif operation == "close":
@@ -54,7 +67,7 @@ def sortContoursByArea(contours, reverse=True):
 
 # Find largest contours
 def xLargestBlobs(mask, top_x=None, reverse=True):
-    # Find all contours from binarised image.
+    # Find all contours from binarised image (masked image).
     # Note: parts of the image that you want to get should be white.
     contours, hierarchy = cv2.findContours(image=mask, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
     n_contours = len(contours)
@@ -81,11 +94,16 @@ def xLargestBlobs(mask, top_x=None, reverse=True):
 # Apply mask to get the right image
 def applyMask(img, mask):
     masked_img = img.copy()
+    # where the mask=0 the returned image must also be 0
     masked_img[mask == 0] = 0
     return masked_img
 
-# Contrast enhancement
+# Contrast enhancement - CLAHE method - Contrast Limited Adaptive Histogram Equalization 
+# the clip is the threshold above which the histogram is capped (see also thesis)
+# tile is the window the method "sees" to process at each step
+# For more info on clip: https://stackoverflow.com/questions/64576472/what-does-clip-limit-mean-exactly-in-opencv-clahe
 def clahe(img, clip=2.0, tile=(8, 8)):
+    # Normalize image
     img = cv2.normalize(
         img,
         None,
@@ -95,11 +113,12 @@ def clahe(img, clip=2.0, tile=(8, 8)):
         dtype=cv2.CV_32F,
     )
     img_uint8 = img.astype("uint8")
+    # Apply clahe
     clahe_create = cv2.createCLAHE(clipLimit=clip, tileGridSize=tile)
     clahe_img = clahe_create.apply(img_uint8)
     return clahe_img
 
-# Image preprocess
+# Combine above into Image preprocessing
 def MammoPreprocess(img):
     # Step 1: Initial crop.
     cropped_img = cropBorders(img=img)
@@ -118,6 +137,7 @@ def MammoPreprocess(img):
     img_pre = img_pre*255
     return img_pre
 
+# Combine into Mask preprocessing
 def MaskPreprocess(mask):
     # Initial crop.
     cropped_mask = cropBorders(img=mask)
@@ -134,7 +154,9 @@ def MaskPreprocess(mask):
 def dataloader(filepath,subset):
     # Tracker
     count = 0
-    # Initiliaze prev
+    # Initiliaze prev, these are for certain cases where two consecutive photos have different masks 
+    # because they correspond to different tumors in the same image.
+    # That is why you need to always remember the last photo you read
     prev_mask = 0
     prev_path = 0
     # Path to downloaded images - path to dataset
@@ -148,7 +170,7 @@ def dataloader(filepath,subset):
             if count==0:
                 count=count+1
                 continue
-            # To ignore " at each other line in csv file
+            # To ignore " at each other line in csv file (open it and you will get it)
             if line[0] == '"':
                 continue
             # Read file and place values
@@ -177,6 +199,7 @@ def dataloader(filepath,subset):
                     img = pixel_array_numpy_img
                     # Preprocess images
                     img = MammoPreprocess(img)
+                    # Save them for use in training and testing
                     image_path = home_path + 'images/' + filepath_img[0] + '.jpg'
                     cv2.imwrite(image_path, img)
             # Read masks
@@ -197,11 +220,13 @@ def dataloader(filepath,subset):
                         # Check for same ID
                         if prev_path == mask_path:
                             print(mask_path)
+                            # Sum current with previous mask in case of multiple tumors
                             summed_mask = np.add(mask, prev_mask)
+                            # Threshold it
                             _, mask = cv2.threshold(src=summed_mask, thresh=1, maxval=255, type=cv2.THRESH_BINARY)
                         # Write path
                         cv2.imwrite(mask_path, mask)
-                        # Prepare for next iteration
+                        # Prepare for next iteration - new prevs
                         prev_path = mask_path
                         prev_mask = mask
             count = count+1
